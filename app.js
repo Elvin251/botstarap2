3712ce33ebebb56498e6dcd46d0bc71383316b33
Elvin251 <emmmdov485@gmail.com>
2024-11-10 18:11:36 +0400

    d

---
+++ app.js
@@ -0,0 +1,12 @@
+//let a="5";console.log(typeof(a))let b= 5;console.log(typeof(b))let c= 4<5;
+
+
+
+//console.log(typeof(c))]
+let your = prompt("adini  yaz");
+let  age = prompt("Yasini yaz");
+let  color = prompt("Sevdiyin reng");
+let  hobby = prompt("Sevdiyin hobby");
+
+
+document.write( "Salam "+ your + ".Sənin " +age +" yasın var." + "en sevdiyin reng    "+color +"dır."+ "Mesqulyetin  "+  hobby +"dır.")
